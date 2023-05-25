ee0a3cceb5b28e3b29e4dfd21a1164a6802edb72
sathya gundoji <56188753+sathya43@users.noreply.github.com>
2023-05-25 21:15:27 +0530

    Added anonymous function to pass to component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,15 +5,14 @@ import Header from '../Header/Header'
 import AddExpenseItemFrom from '../ExpenseTracker/ExpenseItem/AddExpenseItemFrom'
 
 const App = () => {
+  let dummy = () => {}
   return (
     <React.StrictMode>
       <BrowserRouter>
         <Routes>
           <Route
             path='/add'
-            element={
-              <AddExpenseItemFrom onTrue={undefined} onClose={undefined} />
-            }
+            element={<AddExpenseItemFrom onTrue={dummy} onClose={dummy} />}
           ></Route>
           <Route path='/' element={<HomePage />}></Route>
         </Routes>
